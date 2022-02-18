61643d4c2c8e55978f61af8fbbd4ff5bc6fb8756
Ruchika Gosain <sharma.ruchika34@gmail.com>
2022-02-18 12:00:36 -0500

    update the style and readme file

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ const app = express();
 
 app.use(express.static("public"));
 
-// app.get("/", (req, res) => {
-//   res.send("<h1>Hello World!</h1>");
-// });
-
 app.listen(process.env.PORT || 3001, () => {
-  console.log("yes, connected to server...");
+  console.log("Connected to Server...");
 });
